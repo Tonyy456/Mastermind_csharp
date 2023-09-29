f0db11ba85dfecaaa014cecc528bd7c2a1636dd7
Tonyy456 <ajdalesandro0115@gmail.com>
2023-09-29 19:49:49 -0400

    changing interface

---
+++ Mastermind/MastermindController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mastermind
+{
+    /* Author: Anthony D'Alesandro
+     * 
+     * Enum for difficulty. Placed here so avoid excess files.
+     */
+    public enum Difficulty
+    {
+        Easy,
+        Medium, 
+        Hard
+    }
+
+    public class MastermindController
+    {
+        public Difficulty Difficulty { get; set; } = Difficulty.Easy;
+        public MastermindController(Difficulty difficulty)
+        {
+            this.Difficulty = difficulty;
+        }
+
+        public void SetDifficulty(Difficulty difficulty)
+        {
+            Difficulty = difficulty;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to Mastermind!");
+            PromptDifficulty();
+        }
+
+        private void PromptDifficulty()
+        {
+            Console.WriteLine("What difficulty do you want to play?\n (E)asy, (M)edium, (H)ard\n> ");
+            string answer = Console.ReadLine();
+            string cleaned_answer = answer.Trim().ToLower();
+            
+        }
+    }
+}
--- Mastermind/Program.cs
+++ Mastermind/Program.cs
@@ -8,13 +8,13 @@ namespace Mastermind
         static void Main(string[] args)
         {
             // Initialize Game Controller with prameters
-            Console.WriteLine("Welcome to Mastermind!");
 
             // ALLOW USER TO SELECT DIFFICULTY, length of password/# of guesses?
             //GameController controller = new GameController();
             //controller.start(settings);
 
-            Console.WriteLine("\n\n Thank you for playing Mastermind!");
+            MastermindController controller = new MastermindController();
+            controller.Start();
         }
     }
 }
